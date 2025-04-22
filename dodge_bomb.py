a6bc70dfeeee398c473e4525da608ce98af20177
c0a24050b3 <c0a24050b3@edu.teu.ac.jp>
2025-04-22 17:56:53 +0900

    空欄の追加#1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,7 +8,7 @@ import time
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-DELTA ={
+DELTA = {
     pg.K_UP:(0,-5),
     pg.K_DOWN:(0,+5),
     pg.K_LEFT:(-5,0),
@@ -22,11 +22,11 @@ def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
     戻り値：判定結果タプル（横、縦）
     画面内ならTrue､外ならFaluse
     """
-    yoko,tate=True,True #yokototatehoukounohennsuu
+    yoko,tate = True,True #yokototatehoukounohennsuu
     if rct.left <0 or WIDTH < rct.right: #gamennnaidattara
-        yoko=False
-    if rct.top <0 or HEIGHT <rct.bottom :
-        tate=False
+        yoko = False
+    if rct.top <0 or HEIGHT < rct.bottom :
+        tate = False
     return yoko,tate
 
 
@@ -44,8 +44,8 @@ def main():
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
     clock = pg.time.Clock()
-    vx,vy=+5,+5
-    clock =pg.time.Clock()
+    vx,vy = +5,+5
+    clock = pg.time.Clock()
     tmr = 0
     
     def gameover(screen: pg.Surface) -> None: #ゲームオーバーの条件
@@ -54,13 +54,13 @@ def main():
         kuro.set_alpha(150)
         screen.blit(kuro,(0,0)) 
 
-        kk_cry=pg.image.load("fig/8.png")
-        kk_cry1_rect=kk_cry.get_rect(center=(WIDTH//3,HEIGHT//2))
-        kk_cry2_rect=kk_cry.get_rect(center=(2*WIDTH//3,HEIGHT//2))
+        kk_cry = pg.image.load("fig/8.png")
+        kk_cry1_rect = kk_cry.get_rect(center=(WIDTH//3,HEIGHT//2))
+        kk_cry2_rect = kk_cry.get_rect(center=(2*WIDTH//3,HEIGHT//2))
         screen.blit(kk_cry,kk_cry1_rect)
         screen.blit(kk_cry,kk_cry2_rect)
 
-        font =pg.font.Font(None,80)
+        font = pg.font.Font(None,80)
         txt = font.render("Game Over",True,(255,255,255))
         txt_rect = txt.get_rect(center=(WIDTH//2,HEIGHT//2))
 
@@ -71,14 +71,14 @@ def main():
         pg.display.update()
         time.sleep(5)
 
-    def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
-        bb_accs = [a for a in range(1, 11)]
-        bb_imgs = [b for b in range(1, 10)]
+    def init_bb_imgs() -> tuple [list[pg.Surface], list[int]]:
+        bb_accs = [a for a in range (1, 11)]
+        bb_imgs = [b for b in range (1, 10)]
         for r in range(1, 11): 
-            bb_img = pg.Surface((20*r, 20*r))
-            pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
-            avx = vx*bb_accs[min(tmr//500, 9)]
-            bb_img = bb_imgs[min(tmr//500, 9)]
+            bb_img = pg.Surface ((20*r, 20*r))
+            pg.draw.circle (bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+            avx = vx*bb_accs [min(tmr//500, 9)]
+            bb_img = bb_imgs [min(tmr//500, 9)]
     
 
 
@@ -119,7 +119,7 @@ def main():
         #if key_lst[pg.K_RIGHT]:
         #    sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) !=(True,True): #壁の爆弾反射
+        if check_bound(kk_rct) != (True,True): #壁の爆弾反射
             kk_rct.move_in(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
